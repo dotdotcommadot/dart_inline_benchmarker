81b0ed7ca8608e2892005135f8277d51e0802c3c
HVDK <hvdkintersentia@Hanss-MacBook-Pro.local>
2017-09-18 15:03:51 +0200

    Cleans up example

--- example/main.dart
+++ example/main.dart
@@ -1,35 +1,34 @@
 import 'package:inline_benchmarker/inline_benchmarker.dart';
 
-void main() 
-{
-	runMyStuff();
+void main() {
+  
+  runMyStuff();
+  runMyOtherStuff();
 }
 
-void runMyStuff()
-{
+void runMyStuff() {
+
   Benchmark runMyStuffBenchmark = Benchmarker.generate("runMyStuff", group: "MAIN");
   Benchmark forLoopBenchmark = Benchmarker.generate("forLoop", autoRun: false);
-	
-  for (var i = 0; i < 100; i++)
+  
+  for (int i = 0; i < 100; i++)
   {
     forLoopBenchmark.start();
-		
     runMyOtherStuff();
-		
-		forLoopBenchmark.stop();
-	}
-	
-	runMyStuffBenchmark.stop();
-	
-	Benchmarker.report(group: "MAIN");
-	Benchmarker.report();
+    forLoopBenchmark.stop();
+  }
+  
+  runMyStuffBenchmark.stop();
+  
+  Benchmarker.report(group: "MAIN");
+  Benchmarker.report();
 }
 
-void runMyOtherStuff()
-{
-	Benchmark runMyOtherStuffBenchmark = Benchmarker.generate("runMyOtherStuff", indentation: 1, group: "MAIN");
+void runMyOtherStuff() {
 
-	// run
+  Benchmark runMyOtherStuffBenchmark = Benchmarker.generate("runMyOtherStuff", indentation: 1, group: "MAIN");
+
+  // run
   
-	runMyOtherStuffBenchmark.stop();
+  runMyOtherStuffBenchmark.stop();
 }
