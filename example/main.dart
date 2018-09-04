fce1c258e831d634040bd11a06363105aee03794
HVDK <>
2018-09-04 16:03:27 +0200

    Cleans up example

--- example/main.dart
+++ example/main.dart
@@ -1,34 +1,30 @@
 import 'package:inline_benchmarker/inline_benchmarker.dart';
 
 void main() {
-  
   runMyStuff();
   runMyOtherStuff();
 }
 
 void runMyStuff() {
-
-  Benchmark runMyStuffBenchmark = Benchmarker.generate('runMyStuff', group: 'MAIN');
+  Benchmark runMyStuffBenchmark =
+      Benchmarker.generate('runMyStuff', group: 'MAIN');
   Benchmark forLoopBenchmark = Benchmarker.generate('forLoop', autoRun: false);
-  
-  for (int i = 0; i < 100; i++)
-  {
+
+  for (int i = 0; i < 100; i++) {
     forLoopBenchmark.start();
     runMyOtherStuff();
     forLoopBenchmark.stop();
   }
-  
+
   runMyStuffBenchmark.stop();
-  
+
   Benchmarker.report(group: 'MAIN');
   Benchmarker.report();
 }
 
 void runMyOtherStuff() {
+  Benchmark runMyOtherStuffBenchmark =
+      Benchmarker.generate('runMyOtherStuff', indentation: 1, group: 'MAIN');
 
-  Benchmark runMyOtherStuffBenchmark = Benchmarker.generate('runMyOtherStuff', indentation: 1, group: 'MAIN');
-
-  // run
-  
   runMyOtherStuffBenchmark.stop();
 }
